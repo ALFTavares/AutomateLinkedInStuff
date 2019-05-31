254f37a2bbff7840dc589c489a84313cddc2db7d
mouse <alftavares1985@gmail.com>
2019-05-31 12:24:04 +0100

    refactor

--- venv/ClickAccept.py
+++ venv/ClickAccept.py
@@ -9,25 +9,18 @@ class Accept():
         self.driver = driver
 
 
-    def login(self, username, password):
-        username_box = self.driver.find_element_by_id("username")
-        password_box = self.driver.find_element_by_id("password")
-
-        username_box.send_keys(username)
-        password_box.send_keys(password)
-
-        self.driver.find_element_by_class_name('login__form_action_container').click()
-
-
     def move_to_mynetwork(self):
         self.driver.get('https://www.linkedin.com/mynetwork/invitation-manager/')
+        return get_people_to_accept()
 
+
+    def get_people_to_accept(self):
         xpath = '//button[contains(@data-control-name,"accept")]'
         list_of_people_to_accept = self.driver.find_elements_by_xpath(xpath)
         return list_of_people_to_accept
 
 
-    def accept_people(self, list_of_people):
+    def accept_people(self, list_of_people_to_accept):
         for person in list_of_people_to_accept:
             self.driver.execute_script("arguments[0].click();", person)
 
--- venv/main.py
+++ venv/main.py
@@ -19,31 +19,58 @@ def get_linkedin_page():
     driver.get('https://www.linkedin.com/login/')
 
 
+def login(username, password):
+    username_box = driver.find_element_by_id("username")
+    password_box = driver.find_element_by_id("password")
+
+    username_box.send_keys(username)
+    password_box.send_keys(password)
+
+    driver.find_element_by_class_name('login__form_action_container').click()
+
+
+def accept(accept_object):
+    # keep refreshing the page, get the next 10 people in line and accept them
+    while accept_object.has_more():
+        people_to_accept = accept_object.move_to_mynetwork()
+        accept_object.accept_people(people_to_accept)
+
+
+def options_prompt():
+    print('choose an option:')
+    print('1: Accept Invites')
+    return (input('option: '))
+
+
+# add elif conditions to add more options
+# you have to pass the driver as an argument. Don't forget
+def choose_option(option, driver):
+    if option == '1':
+        accept_object = Accept(driver)
+        accept(accept_object)
+    else:
+        print('Invalid option. Choose a valid one')
+        choose_option(options_prompt(), driver)
+
+
 if __name__ == '__main__':
     username = input('insert username\n')
     password = input('insert password\n')
+    driver = None
 
     try:
         # open LinkedIn in login
         driver = open_browser()
         get_linkedin_page()
+        login(username, password)
 
-        # get Accept class
-        accept = Accept(driver)
-
-        # login
-        accept.login(username, password)
-
-        # keep refreshing the page, get the next 10 people in line and accept them
-        while accept.has_more():
-            people_to_accept = accept.move_to_mynetwork()
-            accept.accept_people(people_to_accept)
+        choose_option(options_prompt(), driver)
 
         print('done successfully. ignore errors')
     except Exception as e:
         print('woops! something went wrong\nNow it\'s your chance to correct the problem :)')
         print(e)
     finally:
-        driver.quit()
+        if driver is not None:
+            driver.quit()
         input('press any key to exit...')
-
